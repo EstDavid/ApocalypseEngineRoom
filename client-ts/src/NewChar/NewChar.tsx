80ca36972d882d1bd455cd633314c38aedb36af9
David de Esteban <estdav@protonmail.com>
2024-01-20 09:16:52 +0100

    refactor: typescript of NewChar

--- client-ts/src/NewChar/NewChar.tsx
+++ client-ts/src/NewChar/NewChar.tsx
@@ -2,79 +2,140 @@ import { useState, useEffect } from 'react';
 import './NewChar.css';
 import Select from 'react-select';
 import { AxiosResponse } from 'axios';
+import * as characterService from '../services/characters';
+import { Playbook } from '../types';
 
-function NewChar({userID, client}) {
+function NewChar() {
   const [playbookList, setPlaybookList] = useState([]);
-  const [systemsList, setSystemList] = useState([]);
+  const [systemsList, setSystemList] = useState<
+    { value: string; label: string }[]
+  >([]);
   const [selectedSystem, setSelectedSystem] = useState('');
-  const [selectedPlaybook, setSelectedPlaybook] = useState({});
+  const [selectedPlaybook, setSelectedPlaybook] = useState<Playbook | null>(
+    null
+  );
   const [selectedStatsIndex, setSelectedStatsIndex] = useState(0);
   const [newCharName, setNewCharName] = useState('');
 
-  useEffect(()=> {
-    client.get('playbooks', {withCredentials: true}).then((response:AxiosResponse) => {
+  useEffect(() => {
+    characterService.getPlaybooks().then((response: AxiosResponse) => {
       setPlaybookList(response.data);
-      const sysList = [...new Set(response.data.map(pb => pb.systemName))].map((s) => {return { value: s, label: s };});
+      const sysList = [
+        ...new Set(response.data.map((pb: Playbook) => pb.systemName))
+      ].map((s) => {
+        return { value: s as string, label: s as string };
+      });
       setSystemList(sysList);
     });
-  // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
-  const submitNewChar = () => {
-    if (! selectedPlaybook.name || ! newCharName.length) return;
-    const newCharInfo = {
-      system: selectedPlaybook.systemName,
-      playbook: selectedPlaybook.name,
-      name: newCharName,
-      stats: selectedPlaybook.statsOptions[selectedStatsIndex]
-    };
+  const submitNewChar = async () => {
+    if (selectedPlaybook !== null) {
+      if (!selectedPlaybook.name || !newCharName.length) return;
+      const newCharInfo = {
+        system: selectedPlaybook.systemName,
+        playbook: selectedPlaybook.name,
+        name: newCharName,
+        stats: selectedPlaybook.statsOptions[selectedStatsIndex]
+      };
 
-    client.post('characters', newCharInfo, {withCredentials: true}).then((response:AxiosResponse) => {
-      window.location.replace(`http://localhost:5173/CharacterSheet?CharID=${response.data}`,);
-    });
+      const response = await characterService.create(newCharInfo);
+
+      // TODO check this url creation
+      window.location.replace(
+        `http://localhost:5173/CharacterSheet?CharID=${response.data}`
+      );
+    }
   };
 
   return (
-    <div className='NewChar'>
+    <div className="NewChar">
       <h1>Create a new Character</h1>
-      <div className='NameDiv'>
+      <div className="NameDiv">
         <h2>Character name</h2>
-        <input className='textInput' type="text" name="name" defaultValue={newCharName} onChange={e => setNewCharName(e.target.value)} autoComplete="off"/>
+        <input
+          className="textInput"
+          type="text"
+          name="name"
+          defaultValue={newCharName}
+          onChange={(e) => setNewCharName(e.target.value)}
+          autoComplete="off"
+        />
       </div>
-      <div className='dropdownDiv'>
+      <div className="dropdownDiv">
         <h2>Systems</h2>
-        <Select className='Select' options={systemsList} onChange={(selected) => {setSelectedSystem(selected.value); setSelectedPlaybook({});}} autoFocus={true}/>
+        <Select
+          className="Select"
+          options={systemsList}
+          onChange={(selected) => {
+            if (selected !== null) {
+              setSelectedSystem(selected.value);
+              setSelectedPlaybook(null);
+            }
+          }}
+          autoFocus={true}
+        />
         <h2>Playbooks</h2>
-        <Select className='Select'
-          options={playbookList.filter((pb) => pb.systemName == selectedSystem).map((pb) => {return { value: pb, label: pb.name };})}
-          onChange={(selected) => {setSelectedPlaybook(selected.value), setSelectedStatsIndex(0);}}
-          autoFocus={true}/>
-
+        <Select
+          className="Select"
+          options={playbookList
+            .filter((pb: Playbook) => pb.systemName == selectedSystem)
+            .map((pb: Playbook) => {
+              return { value: pb, label: pb.name };
+            })}
+          onChange={(selected) => {
+            if (selected !== null) {
+              setSelectedPlaybook(selected.value), setSelectedStatsIndex(0);
+            }
+          }}
+          autoFocus={true}
+        />
       </div>
-
-      {selectedPlaybook.description ?
+      {selectedPlaybook && selectedPlaybook.description ? (
         <div>
-          <div className='descDiv'>
-            {selectedPlaybook.description.map((line) => <p key={line}>{line}</p>)}
+          <div className="descDiv">
+            {selectedPlaybook.description.map((line) => (
+              <p key={line}>{line}</p>
+            ))}
           </div>
           <h2>Stat options:</h2>
-          {selectedPlaybook.statOptionsText? <p className='optionText'>{selectedPlaybook.statOptionsText}</p> : ''}
-          <div className='OptionList'>
-            {selectedPlaybook.statsOptions.map((optionList, i) =>
-              <div key={'option ' + i} className='StatOption'>
-                <input type="radio" className='radioInput' checked={i === selectedStatsIndex} onChange={()=>{setSelectedStatsIndex(i);}} />
-                {optionList.map((stat) =>
-                  <div className='statBlock' key={'option-' + i + '-' + stat.name}>
+          {selectedPlaybook.statOptionsText ? (
+            <p className="optionText">{selectedPlaybook.statOptionsText}</p>
+          ) : (
+            ''
+          )}
+          <div className="OptionList">
+            {selectedPlaybook.statsOptions.map((optionList, i) => (
+              <div key={'option ' + i} className="StatOption">
+                <input
+                  type="radio"
+                  className="radioInput"
+                  checked={i === selectedStatsIndex}
+                  onChange={() => {
+                    setSelectedStatsIndex(i);
+                  }}
+                />
+                {optionList.map((stat) => (
+                  <div
+                    className="statBlock"
+                    key={'option-' + i + '-' + stat.name}
+                  >
                     <p>{stat.name}</p>
-                    <div className='statValue'>{stat.value}</div>
-                  </div>)}
-              </div>)}
+                    <div className="statValue">{stat.value}</div>
+                  </div>
+                ))}
+              </div>
+            ))}
           </div>
-          <div className='BtnDiv'>
-            <button id='Submit' onClick={submitNewChar}>Submit</button>
+          <div className="BtnDiv">
+            <button id="Submit" onClick={submitNewChar}>
+              Submit
+            </button>
           </div>
-        </div>:
-        ''}
+        </div>
+      ) : (
+        ''
+      )}
     </div>
   );
 }
