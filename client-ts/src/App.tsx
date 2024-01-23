c43fd5f03697d89782055731c147f6aa3526d1ba
David de Esteban <estdav@protonmail.com>
2024-01-23 18:06:03 +0100

    fix: implement changes after code review

--- client-ts/src/App.tsx
+++ client-ts/src/App.tsx
@@ -1,12 +1,10 @@
 import { useState, useEffect } from 'react';
-import {
-  BrowserRouter, Routes, Route, Link
-} from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import axios from 'axios';
 import Cookies from 'js-cookie';
 
 import { FaUserPlus } from 'react-icons/fa6';
-import { FaHome, FaSignOutAlt  } from 'react-icons/fa';
+import { FaHome, FaSignOutAlt } from 'react-icons/fa';
 
 import CharSheet from './CharSheet/CharSheet';
 import MyChars from './MyChars/MyChars';
@@ -14,7 +12,6 @@ import NewChar from './NewChar/NewChar';
 import Login from './Login/Login';
 import './App.css';
 import { ILogin, ISignup } from './types';
-
 
 const client = axios.create({
   baseURL: 'http://localhost:3000'
@@ -29,24 +26,37 @@ function App() {
     if (cookieID) setUserID(cookieID.slice(3, -1));
   }, []);
 
-  const login : ILogin = (username, password) => {
-    client.post('api/users/login', {username, password} , {withCredentials: true}).then((response) => {
-      if (response.data == 'Wrong connection details' || response.data == 'User Already exists') setLoginIssue(response.data);
-      else setUserID(response.data);
-    }).catch((err) => {
-      console.log(err);
-    });
+  const login: ILogin = (username, password) => {
+    client
+      .post(
+        'api/users/login',
+        { username, password },
+        { withCredentials: true }
+      )
+      .then((response) => {
+        if (response.data._id) {
+          setUserID(response.data._id);
+        } else {
+          setLoginIssue(response.data);
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   };
 
-  const signup : ISignup = (username, password) => {
-    client.post('api/users/signup', {username, password} , {withCredentials: true}).then((response) => {
-      if (response.data == 'Wrong connection details') setLoginIssue(response.data);
-      else setUserID(response.data);
+  const signup: ISignup = (username, password) => {
+    client.post('api/users/signup', { username, password }).then((response) => {
+      if (response.data._id) {
+        setUserID(response.data._id);
+      } else {
+        setLoginIssue(response.data);
+      }
     });
   };
 
   const logout = () => {
-    client.post('logout', {}, {withCredentials: true}).then(() => {
+    client.post('api/users/logout', {}, { withCredentials: true }).then(() => {
       setUserID('');
     });
   };
@@ -54,30 +64,46 @@ function App() {
   return (
     <>
       <BrowserRouter>
-        { userID.length ?
-          <div className='NavBar'>
+        {userID.length ? (
+          <div className="NavBar">
             <Link to="/">
-              <div className='NavBtn'>
+              <div className="NavBtn">
                 <FaHome />
               </div>
             </Link>
             <Link to="NewCharacter">
-              <div className='NavBtn'>
+              <div className="NavBtn">
                 <FaUserPlus />
               </div>
             </Link>
             <Link to="/">
-              <div className='NavBtn' onClick={logout}>
+              <div className="NavBtn" onClick={logout}>
                 <FaSignOutAlt />
               </div>
             </Link>
-          </div>: ''}
+          </div>
+        ) : (
+          ''
+        )}
         <Routes>
-          <Route path="/" element={userID.length
-            ? <MyChars userID={userID} client={client}/>
-            : <Login login={login} signup={signup} loginIssue={loginIssue} />} />
-          <Route path="/NewCharacter" element={<NewChar userID={userID} client={client}/>} />
-          <Route path="/CharacterSheet" element={<CharSheet client={client}/>} />
+          <Route
+            path="/"
+            element={
+              userID.length ? (
+                <MyChars userID={userID} />
+              ) : (
+                <Login login={login} signup={signup} loginIssue={loginIssue} />
+              )
+            }
+          />
+          <Route
+            path="/NewCharacter"
+            element={<NewChar userID={userID} client={client} />}
+          />
+          <Route
+            path="/CharacterSheet"
+            element={<CharSheet client={client} />}
+          />
         </Routes>
       </BrowserRouter>
     </>
