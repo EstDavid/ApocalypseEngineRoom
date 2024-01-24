32d4c33120d9453c618db3bab287e3c23f26e895
David de Esteban <estdav@protonmail.com>
2024-01-24 16:26:34 +0100

    test: implement character test of remaining endpoints

--- server/test/characters.test.ts
+++ server/test/characters.test.ts
@@ -4,7 +4,7 @@ import User from '../model/user';
 import Character from '../model/character';
 import mongoose from 'mongoose';
 import { character1, character2, user1 } from './test.helpers';
-import { IUser } from '../types';
+import { ICharacter, IUser } from '../types';
 
 let cookie: string;
 let user: IUser | null;
@@ -51,7 +51,8 @@ describe('User tests', () => {
     const res = await request
       .post(`${charactersApiUrl}/`,)
       .set('Cookie', cookie)
-      .send(character2);
+      .send(character2)
+      .expect(201);
 
     const characterId = res.body;
 
@@ -69,21 +70,85 @@ describe('User tests', () => {
   test('gets all the characters from a user', async () => {
     const res = await request
       .get(`${charactersApiUrl}/`)
-      .set('Cookie', cookie);
+      .set('Cookie', cookie)
+      .expect(200);
 
     const characterId = res.body;
 
-    console.log(characterId);
+    const createdCharacter = await Character.findById(characterId);
+    if (createdCharacter && user) {
+      expect(createdCharacter).toHaveProperty('_id');
+      expect(createdCharacter.name).toBe(character1.name);
+      expect(createdCharacter.owner).toBe(user._id.toString());
+      expect(createdCharacter.playbook).toBe(character1.playbook);
+    } else {
+      throw new Error('Character was not created');
+    }
+  });
 
+  test('gets a character by Id', async () => {
+    if (user) {
+      const character = await Character.findOne({ owner: user._id });
+      if (character) {
+        const res = await request
+          .get(`${charactersApiUrl}/${character._id}`)
+          .set('Cookie', cookie)
+          .expect(200);
 
-    // const createdCharacter = await Character.findById(characterId);
-    // if (createdCharacter && user) {
-    //   expect(createdCharacter).toHaveProperty('_id');
-    //   expect(createdCharacter.name).toBe(character2.name);
-    //   expect(createdCharacter.owner).toBe(user._id.toString());
-    //   expect(createdCharacter.playbook).toBe(character2.playbook);
-    // } else {
-    //   throw new Error('Character was not created');
-    // }
+        const fetchedCharacter: ICharacter = res.body;
+        if (fetchedCharacter) {
+          expect(fetchedCharacter).toHaveProperty('_id');
+          expect(fetchedCharacter.name).toBe(character1.name);
+          expect(fetchedCharacter.owner).toBe(user._id.toString());
+          expect(fetchedCharacter.playbook).toBe(character1.playbook);
+        } else {
+          throw new Error('Character was not created');
+        }
+      }
+    }
+  });
+
+  test('updates a character', async () => {
+    const newName = 'Big Elf';
+    if (user) {
+      const character = await Character.findOne({ owner: user._id });
+      if (character) {
+        const newData = {
+          updatedField: 'name',
+          newVal: newName
+        };
+        const res = await request
+          .post(`${charactersApiUrl}/${character._id}`)
+          .set('Cookie', cookie)
+          .send(newData)
+          .expect(201);
+
+        const fetchedCharacter: ICharacter = res.body;
+        if (fetchedCharacter) {
+          expect(fetchedCharacter).toHaveProperty('_id');
+          expect(fetchedCharacter.name).toBe(newName);
+          expect(fetchedCharacter.owner).toBe(user._id.toString());
+          expect(fetchedCharacter.playbook).toBe(character1.playbook);
+        } else {
+          throw new Error('Character was not updateds');
+        }
+      }
+    }
+  });
+
+  test('deletes a character', async () => {
+    if (user) {
+      const character = await Character.findOne({ owner: user._id });
+      if (character) {
+        await request
+          .delete(`${charactersApiUrl}/${character._id}`)
+          .set('Cookie', cookie)
+          .expect(204);
+
+        const fetchedCharacter = await Character.findOne({ owner: user._id });
+
+        expect(fetchedCharacter).toBeNull();
+      }
+    }
   });
 });
